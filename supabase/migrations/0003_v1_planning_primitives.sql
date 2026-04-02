264cdfa1ad307f9779f674c75c25df5021d1e836
Ivan Marutsan <vanya.marutsan@gmail.com>
2026-04-03 01:10:15 +0200

    Revert "Merge branch 'main' of https://github.com/IvanMarutsan/personal-assistant-bot"
    This reverts commit 4f0dba46395a1df88889293347c61e6a5116e511, reversing
    changes made to 4df0ba2581bbf287c9175fa46983ad3504c00144.

--- supabase/migrations/0003_v1_planning_primitives.sql
+++
@@ -1,5 +0,0 @@
--- V1 planning primitives compatibility.
--- Add `waiting_response` reason code while keeping existing enum values.
-
-ALTER TYPE public.moved_reason_code
-  ADD VALUE IF NOT EXISTS 'waiting_response';
